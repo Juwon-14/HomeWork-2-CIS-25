56d3463f6b48cc47d848941062c778fae4362036
Juwon-14 <wndnjs9127@gmail.com>
2024-12-11 20:06:41 -0800

    Update main.cpp

--- HomeWork 2 CIS 25/main.cpp
+++ HomeWork 2 CIS 25/main.cpp
@@ -2,13 +2,17 @@
 //  main.cpp
 //  HomeWork 2 CIS 25
 //
-//  Created by 홍주원 on 12/11/24.
+//  Created by Juwon Hong on 12/11/24.
 //
 
+// main.cpp
 #include <iostream>
+#include "DateTime.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    DateTime dt(2024, 12, 11, 14, 30, 45);
+    dt.addDays(1);
+    dt.addSeconds(3600);
+    std::cout << dt.format("Today is Y-MM-DD, and the time is HH:mm:ss") << std::endl;
     return 0;
 }
