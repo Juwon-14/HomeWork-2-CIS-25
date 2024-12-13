dc044b583d68c036b8af27f840c294f796c6c11f
Juwon-14 <wndnjs9127@gmail.com>
2024-12-13 14:04:39 -0800

    Add files via upload

---
+++ HomeWork 2 CIS 25/Time.cpp
@@ -0,0 +1,45 @@
+//
+//  Time.cpp
+//  HomeWork 2 CIS 25
+//
+//  Created by Juwon Hong on 12/13/24.
+//
+
+// Time.cpp
+#include "Time.h"
+
+Time::Time(int h, int m, int s) : hour(h), minute(m), second(s) {}
+
+void Time::addSeconds(int sec) {
+    second += sec;
+    normalize();
+}
+
+void Time::addMinutes(int min) {
+    minute += min;
+    normalize();
+}
+
+void Time::addHours(int hrs) {
+    hour += hrs;
+    normalize();
+}
+
+int Time::getHour() const {
+    return hour;
+}
+
+int Time::getMinute() const {
+    return minute;
+}
+
+int Time::getSecond() const {
+    return second;
+}
+void Time::normalize() {
+    minute += second / 60;
+    second %= 60;
+    hour += minute / 60;
+    minute %= 60;
+    hour %= 24; // Wrap around after 24 hours
+}
