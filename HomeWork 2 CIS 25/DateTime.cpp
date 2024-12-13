4911ec7656cb715a0d463b8d2c853ef8bc4ccb6f
Juwon-14 <wndnjs9127@gmail.com>
2024-12-13 14:03:25 -0800

    Update DateTime.cpp

--- HomeWork 2 CIS 25/DateTime.cpp
+++ HomeWork 2 CIS 25/DateTime.cpp
@@ -6,92 +6,60 @@
 //
 
 #include "DateTime.h"
+#include <sstream>
+#include <iomanip>
+#include <cmath>
 
-Time::Time(int h, int m, int s) : hour(h), minute(m), second(s) {}
+DateTime::DateTime(int y, int m, int d, int h, int min, int s) : Date(y, m, d, h, min, s) {}
 
-void Time::addSeconds(int sec) {
-    second += sec;
-    minute += second / 60;
-    second %= 60;
-    hour += minute / 60;
-    minute %= 60;
-    hour %= 24;
+int DateTime::differenceInSeconds(const DateTime& other) const {
+    int days1 = year * 365 + day;
+    int days2 = other.year * 365 + other.day;
+
+    for (int i = 0; i < month - 1; i++)
+        days1 += daysInMonth(year, i + 1);
+    for (int i = 0; i < other.month - 1; i++)
+        days2 += daysInMonth(other.year, i + 1);
+
+    int diffDays = abs(days1 - days2);
+    int diffSeconds = diffDays * 24 * 60 * 60;
+
+    int totalSec1 = hour * 3600 + minute * 60 + second;
+    int totalSec2 = other.hour * 3600 + other.minute * 60 + other.second;
+
+    return diffSeconds + abs(totalSec1 - totalSec2);
 }
 
-Date::Date(int y, int m, int d) : year(y), month(m), day(d) {}
-
-bool Date::isLeapYear() const {
-    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-}
-
-int Date::daysInMonth() const {
-    static const int daysPerMonth[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    return month == 2 && isLeapYear() ? 29 : daysPerMonth[month - 1];
-}
-
-void Date::addDays(int days) {
-    while (days > 0) {
-        int daysLeftInMonth = daysInMonth() - day;
-        if (days <= daysLeftInMonth) {
-            day += days;
-            return;
-        } else {
-            days -= (daysLeftInMonth + 1);
-            day = 1;
-            month++;
-            if (month > 12) {
-                month = 1;
-                year++;
+string DateTime::format(const string& formatString) const {
+    stringstream ss;
+    for (size_t i = 0; i < formatString.size(); i++) {
+        if (formatString[i] == 'Y' && i + 3 < formatString.size() &&
+            formatString.substr(i, 4) == "YYYY") {
+            ss << setw(4) << setfill('0') << year;
+            i += 3;
+        } else if (formatString[i] == 'M' && i + 1 < formatString.size() &&
+                   formatString.substr(i, 2) == "MM") {
+            ss << setw(2) << setfill('0') << month;
+            i += 1;
+        } else if (formatString[i] == 'D' && i + 1 < formatString.size() &&
+                           formatString.substr(i, 2) == "DD") {
+                    ss << setw(2) << setfill('0') << day;
+                    i += 1;
+                } else if (formatString[i] == 'H' && i + 1 < formatString.size() &&
+                           formatString.substr(i, 2) == "HH") {
+                    ss << setw(2) << setfill('0') << hour;
+                    i += 1;
+                } else if (formatString[i] == 'm' && i + 1 < formatString.size() &&
+                           formatString.substr(i, 2) == "mm") {
+                    ss << setw(2) << setfill('0') << minute;
+                    i += 1;
+                } else if (formatString[i] == 'S' && i + 1 < formatString.size() &&
+                           formatString.substr(i, 2) == "SS") {
+                    ss << setw(2) << setfill('0') << second;
+                    i += 1;
+                } else {
+                    ss << formatString[i];
+                }
             }
+            return ss.str();
         }
-    }
-}
-
-DateTime::DateTime(int y, int m, int d, int h, int min, int sec)
-    : Date(y, m, d), Time(h, min, sec) {}
-
-void DateTime::addSeconds(int sec) {
-    Time::addSeconds(sec);
-    if (hour == 0 && minute == 0 && second == 0) {
-        Date::addDays(1);
-    }
-}
-
-void DateTime::addMinutes(int min) {
-    addSeconds(min * 60);
-}
-
-void DateTime::addHours(int hrs) {
-    addMinutes(hrs * 60);
-}
-
-void DateTime::addDays(int days) {
-    Date::addDays(days);
-}
-
-std::string DateTime::format(const std::string& formatString) const {
-    std::ostringstream oss;
-    for (size_t i = 0; i < formatString.size(); ++i) {
-        if (formatString[i] == 'Y') {
-            oss << year;
-        } else if (formatString[i] == 'M' && formatString[i + 1] == 'M') {
-            oss << std::setw(2) << std::setfill('0') << month;
-            ++i;
-        } else if (formatString[i] == 'D' && formatString[i + 1] == 'D') {
-            oss << std::setw(2) << std::setfill('0') << day;
-            ++i;
-        } else if (formatString[i] == 'H' && formatString[i + 1] == 'H') {
-            oss << std::setw(2) << std::setfill('0') << hour;
-            ++i;
-        } else if (formatString[i] == 'm' && formatString[i + 1] == 'm') {
-            oss << std::setw(2) << std::setfill('0') << minute;
-            ++i;
-        } else if (formatString[i] == 's' && formatString[i + 1] == 's') {
-            oss << std::setw(2) << std::setfill('0') << second;
-            ++i;
-        } else {
-            oss << formatString[i];
-        }
-    }
-    return oss.str();
-}
